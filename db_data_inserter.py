1282e65bfc00648ab9aabf0e0715c6884f0d53cf
Jelle Rodenburg <jellerodenburg@gmail.com>
2022-01-12 22:00:03 +0100

    Implement list products per tag

--- db_data_inserter.py
+++ db_data_inserter.py
@@ -45,9 +45,25 @@ def create_assets():
     print = Product.get_by_id(1)
     statue = Product.get_by_id(2)
     bracelet = Product.get_by_id(3)
-
     # link users to products
     Asset.create(owner=alfred, product=print, product_quantity=5)
     Asset.create(owner=alfred, product=bracelet, product_quantity=3)
     Asset.create(owner=barry, product=statue, product_quantity=1)
     Asset.create(owner=cornelis, product=bracelet, product_quantity=3)
+
+
+def add_tags_to_products():
+    print = Product.get_by_id(1)
+    statue = Product.get_by_id(2)
+    # get tags
+    tag_art = Tag.get_by_id(1)
+    tag_print = Tag.get_by_id(2)
+    tag_red = Tag.get_by_id(3)
+    tag_white = Tag.get_by_id(4)
+    tag_blue = Tag.get_by_id(5)
+    tag_pink = Tag.get_by_id(6)
+
+    print.descriptive_tags.add(
+        [tag_art, tag_print, tag_red, tag_white, tag_blue, tag_pink]
+    )
+    statue.descriptive_tags.add(tag_art)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from models import Asset, Product, User, Tag, Transaction, ProductTag, db
 from rich.console import Console
 from rich.table import Table
 from rich.panel import Panel
-from db_data_inserter import insert_data, create_assets
+from db_data_inserter import insert_data, create_assets, add_tags_to_products
 
 console = Console()
 
@@ -34,7 +34,21 @@ def list_user_products(user_id):
 
 
 def list_products_per_tag(tag_id):
-    ...
+    tag = Tag.get_by_id(tag_id)
+    products_per_tag = [
+        product
+        for product in Product.select()
+        if tag in product.descriptive_tags
+    ]
+    for product in products_per_tag:
+        print(product.name)
+
+    table = Table(
+        title="List of products that have tag:"
+        + f"{tag.name} (tag id: {tag_id}"
+    )
+    table.add_column("Product name")
+    table.add_column("Quantity", justify="right")
 
 
 def add_product_to_catalog(user_id, product):
@@ -57,7 +71,9 @@ def initialize_db():
     db.create_tables([Asset, Product, Tag, Transaction, User, ProductTag])
     insert_data()  # insert product, tag and user data
     create_assets()  # makes users own products (assets)
+    add_tags_to_products()
 
 
 initialize_db()
 list_user_products(1)
+list_products_per_tag(1)
